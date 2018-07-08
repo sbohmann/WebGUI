3cf1be2709ec9786fc18eb368482b1b5a1a887de
Sebastian Bohmann <mail@sebastian-bohmann.com>
2018-07-08 13:51:06 +0200

    Drawing the grid

--- designer/editor.js
+++ designer/editor.js
@@ -1,33 +1,32 @@
-
 class Editor {
     constructor() {
         this._rects = []
         this._createView()
         this._createMouseHandler()
     }
-    
+
     get mainElement() {
         return this._mainElement
     }
-    
+
     get width() {
         return this._canvas.width
     }
-    
+
     get height() {
         return this._canvas.height
     }
-    
+
     addRectangle(rectangle) {
         this._rects.splice(0, 0, rectangle)
         this._paintCanvas()
     }
-    
+
     _createView() {
         this._mainElement = document.createElement('div')
         this._createCanvas()
     }
-    
+
     _createCanvas() {
         this._canvas = document.createElement('canvas')
         this._styleCanvas()
@@ -37,37 +36,30 @@ class Editor {
         }
         setTimeout(() => this._resizeCanvas(), 0)
     }
-    
+
     _styleCanvas() {
         let style = this._canvas.style
-        style.backgroundColor = '#def'
+        style.backgroundColor = '#f3f9ff'
         style.position = 'absolute'
         style.width = '100%'
         style.height = '100%'
     }
-    
+
     _resizeCanvas() {
         this._setCanvasSize()
-        this._callInitializationHandler()
         this._paintCanvas()
     }
-    
+
     _setCanvasSize() {
         this._canvas.width = this._canvas.clientWidth
         this._canvas.height = this._canvas.clientHeight
     }
-    
-    _callInitializationHandler() {
-        if (this._onInitialization !== undefined) {
-            this._onInitialization(this._canvas.width, this._canvas.height)
-        }
-    }
-    
+
     _paintCanvas() {
         let graphicsContext = this._canvas.getContext('2d')
         new EditorPainter(this._rects, this._canvas.width, this._canvas.height, graphicsContext).run()
     }
-    
+
     _createMouseHandler() {
         new MouseEventHandler(
             () => this._rects,
--- designer/editorPainter.js
+++ designer/editorPainter.js
@@ -13,6 +13,7 @@ class EditorPainter {
     
     _paint() {
         this._clearBackground()
+        this._paintGrid()
         this._paintBorder()
         this._paintRects()
     }
@@ -20,17 +21,51 @@ class EditorPainter {
     _clearBackground() {
         this._g.clearRect(0, 0, this._width, this._height);
     }
-    
+
+    _paintGrid() {
+        this._setGridStyle()
+        this._drawGrid()
+    }
+
+    _setGridStyle() {
+        this._g.strokeStyle = '#cc999966'
+        this._g.lineWidth = 1
+    }
+
+    _drawGrid() {
+        const step = 14
+        this._drawVerticalLines(step)
+        this._drawHorizontalLines(step)
+    }
+
+    _drawVerticalLines(step) {
+        for (let x = 0; x < this._width; x += step) {
+            this._g.beginPath()
+            this._g.moveTo(x, 0)
+            this._g.lineTo(x, this._height)
+            this._g.stroke()
+        }
+    }
+
+    _drawHorizontalLines(step) {
+        for (let y = 0; y < this._height; y += step) {
+            this._g.beginPath()
+            this._g.moveTo(0, y)
+            this._g.lineTo(this._width, y)
+            this._g.stroke()
+        }
+    }
+
     _paintBorder() {
         this._setBorderStyle()
         this._paintBorderRect()
     }
-    
+
     _setBorderStyle() {
-        this._g.strokeStyle = '#99cc99'
+        this._g.strokeStyle = '#99cc9966'
         this._g.lineWidth = 12
     }
-    
+
     _paintBorderRect() {
         this._g.beginPath()
         this._g.moveTo(0, 0)
@@ -41,13 +76,13 @@ class EditorPainter {
         this._g.closePath()
         this._g.stroke()
     }
-    
+
     _paintRects() {
         this._forEachRectReverse(rect => {
             rect.paint(this._g)
         })
     }
-    
+
     _forEachRectReverse(func) {
         for (let index = this._rects.length - 1; index >= 0; --index) {
             func(this._rects[index])

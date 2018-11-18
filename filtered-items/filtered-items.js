8ac766d294f65414135768197148cd3c7ce57987
sbohmann <mail@sebastian-bohmann.com>
2018-11-18 22:16:36 +0100

    Added available only checkbox

--- filtered-items/filtered-items.js
+++ filtered-items/filtered-items.js
@@ -6,17 +6,23 @@ class FilteredItems {
         this._createMainElement()
     }
 
-    searchTermChanged(value) {
-        console.log('search term is now [' + value + ']')
-    }
-
     _createMainElement() {
         this._mainElement = document.getElementById('filtered-items-list')
         this._createSearchBar()
     }
 
     _createSearchBar() {
-        this._searchBar = new SearchBar(this)
+        this._searchBar = new SearchBar(
+            value => this._searchTermChanged(value),
+            value => this._availableOnlyChanged(value))
         this._mainElement.appendChild(this._searchBar.mainElement)
+    }
+
+    _searchTermChanged(value) {
+        console.log('search term is now [' + value + ']')
+    }
+
+    _availableOnlyChanged(value) {
+        console.log('available only is now [' + value + ']')
     }
 }
--- filtered-items/search-bar.js
+++ filtered-items/search-bar.js
@@ -1,7 +1,8 @@
 
 class SearchBar {
-    constructor(changeHandler) {
-        this._changeHandler = changeHandler;
+    constructor(searchtermChangeHandler, availableOnlyHandler) {
+        this._searchTermChangeHandler = searchtermChangeHandler
+        this._availableOnlyHandler = availableOnlyHandler
         this._mainElement = document.createElement('div')
         this._createSearchInputField()
         this._createAvailableOnlyCheckbox()
@@ -12,25 +13,36 @@ class SearchBar {
     }
 
     _createSearchInputField() {
+        let paragraph = document.createElement('p')
         let label = document.createElement('label')
         label.textContent = 'Search'
         let textField = document.createElement('input')
         textField.type = 'text'
         textField.oninput = () => this._searchTermChanged(textField.value)
         label.htmlFor = textField
-        this._mainElement.appendChild(label)
-        this._mainElement.appendChild(textField)
+        paragraph.appendChild(label)
+        paragraph.appendChild(textField)
+        this._mainElement.appendChild(paragraph)
     }
 
     _searchTermChanged(value) {
         value = value.trim().toLowerCase()
         if (this._previousSearchTerm !== value) {
-            this._changeHandler.searchTermChanged(value)
+            this._searchTermChangeHandler(value)
         }
         this._previousSearchTerm = value
     }
 
     _createAvailableOnlyCheckbox() {
-
+        let paragraph = document.createElement('p')
+        let label = document.createElement('label')
+        label.textContent = 'Available Only'
+        let checkbox = document.createElement('input')
+        checkbox.type = 'checkbox'
+        checkbox.onchange = () => this._availableOnlyHandler(checkbox.checked)
+        label.htmlFor = checkbox
+        paragraph.appendChild(label)
+        paragraph.appendChild(checkbox)
+        this._mainElement.appendChild(paragraph)
     }
 }
